fdb96063db4be0174f5728c773efcdfb067bf067
David Trudgian <david.trudgian@sylabs.io>
2021-05-19 16:27:59 -0500

    Use SingularityCE and -ce package name consistently

--- replacements.py
+++ replacements.py
@@ -1,20 +1,28 @@
 def variableReplace(app, docname, source):
     """
-    Takes the source on rst and replaces all the needed variables declared on variable_replacements structure
+    Takes the source on rst and replaces all the needed variables declared on 
+    variable_replacements structure
     """
     result = source[0]
     for key in app.config.variable_replacements:
         result = result.replace(key, app.config.variable_replacements[key])
     source[0] = result
 
-#Add the needed variables to be replaced either on code or on text on the next dictionary structure.
+
+# Add the needed variables to be replaced either on code or on text on the next
+# dictionary structure.
 variable_replacements = {
-    "{InstallationVersion}" : "3.7.0",
-    "\{version\}" : "3.7",
-    "\{adminversion\}" : "3.7"
+    "{InstallationVersion}": "3.8.0",
+    "\{version\}": "3.8",
+    "\{adminversion\}": "3.8",
+    # The 'Singularity' noun is now a replacement so we can have
+    # {Singularity}  rather than bare 'Singularity'... and Sylabs can
+    # replace to SingularityPRO so that it is clearer where docs
+    # diverge a bit from CE<->PRO due to long-term backports etc.
+    "{Singularity}": "SingularityCE",
 }
 
-def setup(app):
-   app.add_config_value('variable_replacements', {}, True)
-   app.connect('source-read', variableReplace)
 
+def setup(app):
+    app.add_config_value('variable_replacements', {}, True)
+    app.connect('source-read', variableReplace)
